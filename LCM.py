300793c50545aac56b32d624b12572994c4f8c2d
programlearner1 <113779810+programlearner1@users.noreply.github.com>
2022-12-04 20:39:08 +0530

    LCM calculation program

--- LCM.py
+++ LCM.py
@@ -11,4 +11,4 @@ def compute_lcm(x,y):
     return lcm
 num1 = int(input("num1: "))
 num2 = int(input("num2: "))
-print("The L.C.M. is a ", compute_lcm (num1,num2))
+print("The L.C.M. is ", compute_lcm (num1,num2))
